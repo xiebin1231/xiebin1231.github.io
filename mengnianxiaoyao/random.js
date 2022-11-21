15d3ccb38df17c72ca47fe197256ce7194f4e54f
xiebin1231 <1120448506@qq.com>
2022-11-21 23:33:51 +0800

    Site updated: 2022-11-21 23:33:49

--- mengnianxiaoyao/random.js
+++ mengnianxiaoyao/random.js
@@ -1,1 +1,1 @@
-var posts=["2022/11/20/第一篇文章/","2022/11/20/第三篇文章/","2022/11/20/第二篇文章/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
+var posts=["2022/11/20/第三篇文章/","2022/11/20/第二篇文章/","2022/11/20/第一篇文章/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
